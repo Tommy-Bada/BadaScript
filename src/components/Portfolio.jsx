dba35d15290988b31bd74ad2dbf863017faaa660
Tomiwa Bada <tomiwabd@gmail.com>
2023-09-24 20:02:17 +0100

    Updated portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,31 @@
 import "./Portfolio.css";
 import Project from "./Project";
 import geegpay from "../assets/Geegpay.png";
+import countrydetailer from "../assets/countrydetailer.png";
+import electpoll from "../assets/electpoll.png";
+import expensetracker from "../assets/expensetracker.png";
+import metamask from "../assets/metamask.png";
+import plwlsn from "../assets/plwlsn.png";
+import sanwo from "../assets/sanwo.png";
+import uigpa from "../assets/uigpa.png";
 
 function Portfolio() {
   const projects = [
+    {
+      image: plwlsn,
+      title: "PL-WLSN Stores",
+      description: "A Men's fashion store for exclusive wears for PLWLSN Brand",
+      url: "https://plwlsn.netlify.app",
+      stacks: "React + CSS + Cloudinary + Paystack",
+    },
+    {
+      image: metamask,
+      title: "Metamask",
+      description:
+        "A web3 real estate app that allows users to rent properties in the metaverse using etherium",
+      url: "https://rentnft.netlify.app",
+      stacks: "React + CSS",
+    },
     {
       image: geegpay,
       title: "Geegpay",
@@ -11,6 +33,43 @@ function Portfolio() {
         "Responsive landing page replication, with DOM manipulation for custom content for Nigeria and Kenya. ",
       url: "https://goldfinance.netlify.app",
       stacks: "HTML + CSS + JavaScript",
+    },
+    {
+      image: countrydetailer,
+      title: "Country API",
+      description: "A counrty detailer app that fetches data froman API",
+      url: "https://countrydetailer.netlify.app",
+      stacks: "HTML + CSS + JavaScript",
+    },
+    {
+      image: electpoll,
+      title: "Electpoll",
+      description:
+        "A voting app that is used for conducting elections and polls in institutions and organizations",
+      url: "https://electpoll.netlify.app",
+      stacks: "HTML + CSS + JavaScript + Airtable",
+    },
+    {
+      image: expensetracker,
+      title: "Expense Tracker",
+      description:
+        "An expense tracker app that helps you keep track of your expenses",
+      url: "https://transak.netlify.app",
+      stacks: "HTML + CSS + JavaScript",
+    },
+    {
+      image: uigpa,
+      title: "CGPA Calculator",
+      description:
+        "A CGPA calculator that helps you calculate your CGPA with ease. Built for the academic team of the Department of Mathematics, University of Ibadan.",
+      url: "https://uigpa.netlify.app",
+      stacks: "HTML + CSS + JavaScript",
+    },
+    {
+      image: sanwo,
+      title: "Sanwo Landing Page",
+      description:
+        "A landing page for a fintech in Ibadan that helps with escrow payments",
     },
   ].map((project) => (
     <Project
